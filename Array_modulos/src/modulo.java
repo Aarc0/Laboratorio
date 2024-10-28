854dc4c0862c2f0d850879af37c126cd522576f2
andre <andre@DESKTOP-2D3OFPM>
2024-10-28 20:50:22 +0100

    moduls

---
+++ Array_modulos/src/modulo.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class modulo 
+{
+	public static void main(String[]args)
+	{
+		Scanner n = new Scanner(System.in);
+		System.out.println("Ingrese cuantas matrices quiere crear");
+		int nm = n.nextInt();
+		//se crea una variable llamada "nm" que guarda el valor del número de matrices a realizar
+		nmatriz(nm);
+		//La variable nm se incerta dentro del argumento de "nmatriz" indicandolo el número de veces que tiene que repetir una tarea
+		
+		
+		
+		System.out.println("Ingrese el número de fílas para la matriz");
+		
+			
+		
+		
+		
+	}
+	public static int nmatriz(int N)
+	{
+		Scanner m = new Scanner(System.in);
+		System.out.println("Por favor incerte el número de filas que contendrá la matriz: ");
+		int filas = m.nextInt();
+		System.out.println("Por favor incerte el número de columnas que contendrá la matriz: ");
+		int columnas = m.nextInt();
+		//Aquí pido ingresar el número de filas y columnas que contendrá la matriz
+		
+		
+		for(int i = 0; i<N; i++)
+		//Este bucle se ejecutara el número de veces que "i" sea menor que "N", siendo "N" el número de matrices a crear
+		{
+			array(filas, columnas);
+		}
+		return N;
+	}
+	public static int array(int f, int c)
+	{
+		Scanner m = new Scanner(System.in);
+		int array[][] = new int[f][c];
+		for(int i = 0; i < f; i++)
+		{
+			for(int j = 0; i<c; j++)
+			{
+				array[i][j] = m.nextInt();
+			}
+		}
+		
+		
+		return array(f, c);
+	}
+	
+
+}
