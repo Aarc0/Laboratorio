f86d9353d9be6283beb7e419eca78d507675513d
andre <andre@DESKTOP-2D3OFPM>
2024-11-09 01:09:58 +0100

    Cuenta primos completo

--- Primos_entre_Nº's/src/CuentaPrimos.java
+++ Primos_entre_Nº's/src/CuentaPrimos.java
@@ -6,28 +6,35 @@ public class CuentaPrimos
 	public static void main(String[] args) 
 	{
 		Scanner n = new Scanner(System.in);
-		System.out.println("Ingrese el numero hasta el cual se quieran contar los numeros primos que hay");
+		System.out.println("Ingrese el numero hasta el cual se quieran contar los numeros primos que hay: ");
 		int num = n.nextInt();
 		boolean a = true;
-		if(num==1)
+		int p = 0;
+		int np = 0;
+		for(int j = 1;j<=num;j++)
 		{
-			System.out.println(num + " no es primo");
-			n.close();
-			return;
-		}
-		for(int j = 1;j<num;j++)
-		{
-			if(num%j==0 && num/j ==1)
+			if(j == 1) a = false;
+			for(int i = 2; i<j;i++)
 			{
-				a = false;
-				System.out.println(j+" no es primo");
+				if(j%i==0)
+				{
+					a = false;
+				}
 			}
-			else if(num%j != 0)
+			if (a == true)
 			{
-				a = true;
 				System.out.println(j+" es primo");
+				p++;
 			}
+			else if (a == false)
+			{
+				System.out.println(j+" no es primo");
+				np++;
+				a = true;
+			}
+			
 		}
+		System.out.println("Hay "+p + " numeros primos" );
+		System.out.println("Hay "+np + " numeros que no son primos" );
 	}
-
 }
