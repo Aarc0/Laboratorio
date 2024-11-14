1dceb5e0e9c3fff99c8296f20cdfb438cc6e604f
andre <andre@DESKTOP-2D3OFPM>
2024-11-14 21:16:39 +0100

    InformaVector

---
+++ InformaVector/src/InformaVector.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InformaVector 
+{
+	public static void main(String[] args) 
+	{
+		Scanner n = new Scanner(System.in);
+		System.out.println("Ingrese el tamaño del vector");
+		int T = n.nextInt();
+		int [] a = new int[T];
+		for(int i = 0; i<T; i++)
+		{
+			int r = (int)(Math.random()*99);
+			
+		}
+		
+		
+	}
+}
