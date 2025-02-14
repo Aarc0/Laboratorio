2ae89b85b4444a51fdbedcb3fb50fd02f02344cf
andre <andre@DESKTOP-2D3OFPM>
2025-02-14 07:27:29 +0100

    poke

---
+++ FP_T1/src/Pokemon/Entrenador.java
@@ -0,0 +1,23 @@
+package Pokemon;
+
+public class Entrenador 
+{
+	private String nombre;
+	private int pokeball;	
+	
+	public String getNombre() 
+	{
+		return nombre;
+	}
+	public void setNombre(String nombre) 
+	{
+		this.nombre = nombre;
+	}
+	
+	public Entrenador(String nombre) 
+	{
+		this.nombre = nombre;
+
+	}
+	
+}
---
+++ FP_T1/src/Pokemon/Pokemons.java
@@ -0,0 +1,33 @@
+package Pokemon;
+
+public class Pokemons 
+{
+	private String nombre;
+	private String tipo;
+	
+	public void Setnombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+	public String Getnombre()
+	{
+		return nombre;
+	}
+	public void Settipo(String tipo)
+	{
+		this.tipo = tipo;
+	}
+	public String Gettipo()
+	{
+		return tipo;
+	}
+	
+	public Pokemons(String nombre, String tipo) 
+	{
+		this.nombre = nombre;
+		this.tipo = tipo;
+	}
+	
+	
+}
+
