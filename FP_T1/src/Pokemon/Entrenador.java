4b2df2d9055671cd34088775c80b64a862206f48
Andres <Andres@e-rojas>
2025-02-17 12:56:08 +0100

    Continuar

--- FP_T1/src/Pokemon/Entrenador.java
+++ FP_T1/src/Pokemon/Entrenador.java
@@ -3,7 +3,9 @@ package Pokemon;
 public class Entrenador 
 {
 	private String nombre;
-	private int pokeball;	
+	private String pok1;
+	private String pok2;
+	private String pok3;
 	
 	public String getNombre() 
 	{
@@ -14,10 +16,37 @@ public class Entrenador
 		this.nombre = nombre;
 	}
 	
+	public String getPok1() {
+		return pok1;
+	}
+	public void setPok1(String pok1) 
+	{
+		this.pok1 = pok1;
+	}
+	
+	public String getPok2() 
+	{
+		return pok2;
+	}
+	public void setPok2(String pok2) 
+	{
+		this.pok2 = pok2;
+	}
+	
+	public String getPok3() 
+	{
+		return pok3;
+	}
+	public void setPok3(String pok3) 
+	{
+		this.pok3 = pok3;
+	}
+	
 	public Entrenador(String nombre) 
 	{
 		this.nombre = nombre;
-
 	}
+	
+	
 	
 }
--- FP_T1/src/Pokemon/Main.java
+++ FP_T1/src/Pokemon/Main.java
@@ -2,8 +2,6 @@ package Pokemon;
 
 public class Main 
 {
-	
-	
 	public static void main(String[] args)
 	{
 		Pokemons po1 = new Pokemons("Pikachu","Eléctrico");
@@ -11,22 +9,27 @@ public class Main
 		Pokemons po3 = new Pokemons("Charmander","Fuego");
 		Pokemons po4 = new Pokemons("Squirtle","Agua");
 		Pokemons po5 = new Pokemons("Caterpie","Bicho");
-		Entrenador e1 = new Entrenador("Ash");
+		Entrenador entrenador = new Entrenador("Ash");
 		
 		
-		
+		boolean b = true;
+		int x = 0;
+		int aux = 0;
 		int [] pkb = new int[6];
 		//Inicializo la energía de todas las pokeballs a 10
 		for(int i = 0; i<pkb.length;i++)
 		{
 			pkb[i] = 10; 
 		}
-		
+		System.out.println("== Estado inicial de los Pokémon ==");
 		System.out.println("Pokemon: "+po1.Getnombre()+" Tipo: "+po1.Gettipo());
 		System.out.println("Pokemon: "+po2.Getnombre()+" Tipo: "+po2.Gettipo());
-		System.out.println("Pokemon: "+po3.Getnombre()+" Tipo: "+po3.Gettipo());
-			
-	
+		System.out.println("Pokemon: "+po3.Getnombre()+" Tipo: "+po3.Gettipo()+"\n\n");
+		
+		System.out.println("== Capturando Pokémon ==");
+		
+		
+		
 	}
 	
 }
