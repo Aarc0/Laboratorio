f46b3b9ce87b32bbaf4805cf193393f4591bb1d4
andre <andre@DESKTOP-2D3OFPM>
2025-02-18 22:42:59 +0100

    Pokemon hecho creo

--- FP_T1/src/Pokemon/Entrenador.java
+++ FP_T1/src/Pokemon/Entrenador.java
@@ -3,9 +3,12 @@ package Pokemon;
 public class Entrenador 
 {
 	private String nombre;
-	private String pok1;
-	private String pok2;
-	private String pok3;
+	private String pok1n;
+	private String pok2n;
+	private String pok3n;
+	private String pok1t;
+	private String pok2t;
+	private String pok3t;
 	
 	public String getNombre() 
 	{
@@ -15,36 +18,62 @@ public class Entrenador
 	{
 		this.nombre = nombre;
 	}
-	
+	/////////////////////////////////////
 	public String getPok1() {
-		return pok1;
+		return pok1n;
 	}
 	public void setPok1(String pok1) 
 	{
-		this.pok1 = pok1;
+		this.pok1n = pok1;
 	}
-	
+	/////////////////////////////////////
 	public String getPok2() 
 	{
-		return pok2;
+		return pok2n;
 	}
 	public void setPok2(String pok2) 
 	{
-		this.pok2 = pok2;
+		this.pok2n = pok2;
 	}
-	
+	/////////////////////////////////////
 	public String getPok3() 
 	{
-		return pok3;
+		return pok3n;
 	}
 	public void setPok3(String pok3) 
 	{
-		this.pok3 = pok3;
+		this.pok3n = pok3;
 	}
-	
+	/////////////////////////////////////
 	public Entrenador(String nombre) 
 	{
 		this.nombre = nombre;
+	}
+	/////////////////////////////////////
+	public String getPok1t() 
+	{
+		return pok1t;
+	}
+	public void setPok1t(String pok1t) 
+	{
+		this.pok1t = pok1t;
+	}
+	/////////////////////////////////////
+	public String getPok2t() {
+		return pok2t;
+	}
+	public void setPok2t(String pok2t) 
+	{
+		this.pok2t = pok2t;
+	}
+	/////////////////////////////////////
+	public String getPok3t() 
+	{
+		return pok3t;
+	}
+	public void setPok3t(String pok3t) 
+	{
+		this.pok3t = pok3t;
 	}
 	
 	
--- FP_T1/src/Pokemon/Main.java
+++ FP_T1/src/Pokemon/Main.java
@@ -11,25 +11,90 @@ public class Main
 		Pokemons po5 = new Pokemons("Caterpie","Bicho");
 		Entrenador entrenador = new Entrenador("Ash");
 		
-		
-		boolean b = true;
 		int x = 0;
-		int aux = 0;
+		int y = 0;
+		int z = 0;
+		String aux;
 		int [] pkb = new int[6];
 		//Inicializo la energía de todas las pokeballs a 10
 		for(int i = 0; i<pkb.length;i++)
 		{
 			pkb[i] = 10; 
 		}
+		
 		System.out.println("== Estado inicial de los Pokémon ==");
 		System.out.println("Pokemon: "+po1.Getnombre()+" Tipo: "+po1.Gettipo());
 		System.out.println("Pokemon: "+po2.Getnombre()+" Tipo: "+po2.Gettipo());
 		System.out.println("Pokemon: "+po3.Getnombre()+" Tipo: "+po3.Gettipo()+"\n\n");
 		
 		System.out.println("== Capturando Pokémon ==");
+		// X es contador de pokeballs
+		// Y es contador de intentos
+		// Z Pokemon por el que vamos
 		
-		
-		
+		while(x<pkb.length)
+		{
+			////////////////////////////////////////
+			if(z == 0)	aux = po1.Getnombre();
+			else if(z == 1)	aux = po2.Getnombre();
+			else aux = po3.Getnombre();
+			///////////////////////////////////////
+			
+			while(y<=1)
+			{
+				System.out.println("La captura de "+ aux +" falló.");
+				pkb[x] -= 4;
+				y++;
+			}
+			if(pkb[x] < 3)
+			{
+				x++;
+			}
+			if(y == 2)
+			{
+				System.out.println("¡Captura exitosa! "+aux+" ahora es tuyo.\n");
+				if(z == 0)	{entrenador.setPok1(aux); entrenador.setPok1t(po1.Gettipo());}
+				else if(z == 1)	{entrenador.setPok2(aux); entrenador.setPok2t(po2.Gettipo());}
+				else {entrenador.setPok3(aux); entrenador.setPok3t(po3.Gettipo());}
+				y = 0;
+				z++;
+			}
+			
+			//Equipo lleno
+			if(z == 3)
+			{
+				aux = po4.Getnombre();
+				System.out.println("== Captura con equipo lleno ==");
+				while(y<=1)
+				{
+					System.out.println("La captura de "+ aux +" falló.");
+					pkb[x] -= 4;
+					y++;
+				}
+				if(pkb[x] < 3)
+				{
+					x++;
+				}
+				if(y == 2) System.out.println("¡Captura fallida! el entrenador no tiene suficiente espacio.");
+				break;
+			}
+		}
+		x = 0;
+		for(int i = 0; i<pkb.length;i++)
+		{
+			pkb[i] = 0;
+			x++;
+			if(x == 5)
+			{
+				System.out.println("\nNo hay Pokéballs disponibles para capturar a "+po5.Getnombre()+"\n");
+			}
+		}
+		System.out.println("== Estado final de los Pokémon de Ash ==");
+		for(int i = 0;i<3;i++)
+		{
+			if(i == 0) System.out.println("Pokemon: "+entrenador.getPok1()+" Tipo: "+entrenador.getPok1t());
+			else if(i == 1) System.out.println("Pokemon: "+entrenador.getPok2()+" Tipo: "+entrenador.getPok2t());
+			else System.out.println("Pokemon: "+entrenador.getPok3()+" Tipo: "+entrenador.getPok3t());
+		}
 	}
-	
 }
