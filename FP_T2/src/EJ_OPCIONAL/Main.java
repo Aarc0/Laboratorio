a830a261b66a3115b781f55647e96ab29a4e971c
Andres <Andres@172.19.161.42>
2025-03-27 09:14:19 +0100

    Excepciones

--- FP_T2/src/EJ_OPCIONAL/Main.java
+++ FP_T2/src/EJ_OPCIONAL/Main.java
@@ -6,8 +6,8 @@ public class Main
 	{
 		Biblioteca [] pub = new Biblioteca[4];
 		pub[0] = new Revista(123  ,2001  ,false  ,5);
-		pub[1] = new Revista(1234 ,2002  ,true  ,6);
-		pub[2] = new Libro(12345  ,2003  ,true  ,"Pepe");
+		pub[1] = new Revista(1234 ,2002  ,true   ,6);
+		pub[2] = new Libro(12345  ,2003  ,true   ,"Pepe");
 		pub[3] = new Libro(123456 ,2004  ,false  ,"Juan");
 		
 		System.out.println("En este momento hay un total de "+publi_prestada(pub)+ " libros prestados\n");
---
+++ FP_T3/src/EJ_Expecion/Main.java
@@ -0,0 +1,14 @@
+package EJ_Expecion;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 0;
+		
+		
+
+	}
+
+}
